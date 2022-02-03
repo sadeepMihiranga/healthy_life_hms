5c2ee94842efcd118beb1213009c5ddde1038f12
sadeep <sadeep@surecore.co>
2022-02-03 19:54:56 +0530

    Initial commit

---
+++ src/main/java/lk/healthylife/hms/HealthyLifeHmsApplication.java
@@ -0,0 +1,13 @@
+package lk.healthylife.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyLifeHmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyLifeHmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/healthylife/hms/HealthyLifeHmsApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.healthylife.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyLifeHmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
